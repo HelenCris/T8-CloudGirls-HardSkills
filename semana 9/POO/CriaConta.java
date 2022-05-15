74de94eeb559a6f6ab9e3a6090e426fbab5e4440
Helen Cris <helencrisfernandes@hotmail.com>
2022-05-14 21:28:02 -0300

    modulo de POO concluido com sucesso

--- semana 9/POO/CriaConta.java
+++ semana 9/POO/CriaConta.java
@@ -1,36 +1,22 @@
 package POO;
 
-public class CriaConta { 
+public class CriaConta {
+
     public static void main(String[] args) {
-        Conta primeiraConta = new Conta();
-        primeiraConta.saldo = 200; 
-        System.out.println(primeiraConta.saldo); 
+        Conta primeiraConta = new Conta(6646,969060);
+        primeiraConta.deposita(200);
+        System.out.println(primeiraConta.getSaldo());
 
-        primeiraConta.saldo += 100;
-        System.out.println(primeiraConta.saldo);
+        primeiraConta.deposita(100);
+        System.out.println(primeiraConta.getSaldo());
 
-        Conta segundaConta = new Conta();
-        segundaConta.saldo = 300;
+        Conta segundaConta = new Conta(6645,969061);
+        segundaConta.deposita(50);
 
-        System.out.println("primeira conta tem " + primeiraConta.saldo);
-        System.out.println("segunda conta tem " + segundaConta.saldo);
+        System.out.println("primeira conta tem " + primeiraConta.getSaldo());
+        System.out.println("segunda conta tem " + segundaConta.getSaldo());
 
-        segundaConta.agencia = 146;
-        System.out.println(primeiraConta.agencia);
-        System.out.println(primeiraConta.numero);
-
-            System.out.println(segundaConta.agencia);
-
-        segundaConta.agencia = 146;
-        System.out.println("agora a segunda conta está na agencia " + segundaConta.agencia);
-
-        if(primeiraConta == segundaConta) {
-            System.out.println("mesma conta");
-        } else {
-            System.out.println("contas diferentes");
-        }
-
-        System.out.println(primeiraConta);
-        System.out.println(segundaConta);
+        System.out.println("referenciar: " + primeiraConta);
     }
+
 }
